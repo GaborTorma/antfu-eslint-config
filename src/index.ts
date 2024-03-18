18b50a2314dc2e61e90675c443c8c75fe7da85ab
Gábor Torma <gabor@torma.dev>
2024-03-18 15:16:53 +0100

    fix: export default antfu

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import config from '@antfu/eslint-config'
 import { defu } from 'defu'
 import { defaultOptions } from './options'
 
-export function antfu(options?: OptionsConfig & FlatConfigItem, ...userConfigs: Awaitable<UserConfigItem | UserConfigItem[]>[]): Promise<UserConfigItem[]> {
+export default function antfu(options?: OptionsConfig & FlatConfigItem, ...userConfigs: Awaitable<UserConfigItem | UserConfigItem[]>[]): Promise<UserConfigItem[]> {
   return config(defu(
     options,
     defaultOptions,
