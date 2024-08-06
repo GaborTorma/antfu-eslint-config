b4356d5057e1fcd91722dc6fe1c5e54153588c66
Gábor Torma <gabor@torma.dev>
2024-08-06 17:29:57 +0200

    fix: refactor rules

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import type { Awaitable, OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
 import config from '@antfu/eslint-config'
 import { defu } from 'defu'
-import { consoleRules, defaultOptions, defaultRules, packageJsonRules } from './options'
+import { defaultOptions, packageJsonRules, styleMaxLenRules } from './options'
 
 export default async function antfu(options?: OptionsConfig & TypedFlatConfigItem, ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any> | Linter.FlatConfig[]>[]): Promise<FlatConfigComposer<TypedFlatConfigItem>> {
   return config(
@@ -11,9 +11,8 @@ export default async function antfu(options?: OptionsConfig & TypedFlatConfigIte
       options,
       defaultOptions,
     ),
-    defaultRules,
-    consoleRules,
     packageJsonRules,
+    styleMaxLenRules,
     ...userConfigs,
   )
 }
--- src/options.ts
+++ src/options.ts
@@ -1,20 +1,5 @@
 import type { OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
 
-export const styleMaxLen: TypedFlatConfigItem['rules'] = {
-  // https://eslint.style/rules/js/max-len
-  'style/max-len': ['warn', {
-    code: 125, // https://eslint.style/rules/js/max-len#code
-    tabWidth: 2, // https://eslint.style/rules/js/max-len#tabwidth
-    ignoreComments: true, // https://eslint.style/rules/js/max-len#ignorecomments
-    ignoreTrailingComments: true, // https://eslint.style/rules/js/max-len#ignoretrailingcomments
-    ignoreUrls: true, // https://eslint.style/rules/js/max-len#ignoreurls
-    ignoreStrings: true, // https://eslint.style/rules/js/max-len#ignorestrings
-    ignoreTemplateLiterals: true, // https://eslint.style/rules/js/max-len#ignoretemplateliterals
-    ignoreRegExpLiterals: true, // https://eslint.style/rules/js/max-len#ignoreregexpliterals
-    ignorePattern: `^(\\s+|export\\s(default\\s)?(async\\s)?)function`, // https://eslint.style/rules/js/max-len#ignorepattern
-  }],
-}
-
 export const defaultOptions: OptionsConfig & TypedFlatConfigItem = {
   // https://github.com/antfu/eslint-config?tab=readme-ov-file#type-aware-rules
   typescript: {
@@ -30,7 +15,6 @@ export const defaultOptions: OptionsConfig & TypedFlatConfigItem = {
       ],
       'ts/method-signature-style': ['error', 'method'], // https://typescript-eslint.io/rules/method-signature-style#method
       'ts/strict-boolean-expressions': 'off', // https://typescript-eslint.io/rules/strict-boolean-expressions
-      ...styleMaxLen,
     },
   },
   vue: {
@@ -49,7 +33,6 @@ export const defaultOptions: OptionsConfig & TypedFlatConfigItem = {
         singleline: 100, // https://eslint.vuejs.org/rules/max-attributes-per-line#singleline-3
         multiline: 1, // https://eslint.vuejs.org/rules/max-attributes-per-line#multiline-2
       }],
-      ...styleMaxLen,
 
     },
   },
@@ -60,23 +43,29 @@ export const defaultOptions: OptionsConfig & TypedFlatConfigItem = {
         'warn',
         { args: 'after-used', argsIgnorePattern: '^_', vars: 'all', varsIgnorePattern: '^_' },
       ],
-      ...styleMaxLen,
+      'object-shorthand': ['error', 'always', {
+        avoidQuotes: false, // https://eslint.org/docs/latest/rules/object-shorthand#avoidquotes
+      }],
+      'no-console': 'off', // https://eslint.org/docs/latest/rules/no-console
     },
   },
   formatters: true, // https://github.com/antfu/eslint-config?tab=readme-ov-file#formatters
 }
 
-export const consoleRules: TypedFlatConfigItem = {
+export const styleMaxLenRules: TypedFlatConfigItem = {
+  files: ['**/*.?([cm])js', '**/*.?([cm])jsx', '**/*.?([cm])ts', '**/*.?([cm])tsx', '**/*.vue'],
   rules: {
-    'no-console': 'off', // https://eslint.org/docs/latest/rules/no-console
-  },
-}
-
-export const defaultRules: TypedFlatConfigItem = {
-  rules: {
-    // https://eslint.org/docs/latest/rules/object-shorthand
-    'object-shorthand': ['error', 'always', {
-      avoidQuotes: false, // https://eslint.org/docs/latest/rules/object-shorthand#avoidquotes
+    // https://eslint.style/rules/js/max-len
+    'style/max-len': ['warn', {
+      code: 125, // https://eslint.style/rules/js/max-len#code
+      tabWidth: 2, // https://eslint.style/rules/js/max-len#tabwidth
+      ignoreComments: true, // https://eslint.style/rules/js/max-len#ignorecomments
+      ignoreTrailingComments: true, // https://eslint.style/rules/js/max-len#ignoretrailingcomments
+      ignoreUrls: true, // https://eslint.style/rules/js/max-len#ignoreurls
+      ignoreStrings: true, // https://eslint.style/rules/js/max-len#ignorestrings
+      ignoreTemplateLiterals: true, // https://eslint.style/rules/js/max-len#ignoretemplateliterals
+      ignoreRegExpLiterals: true, // https://eslint.style/rules/js/max-len#ignoreregexpliterals
+      ignorePattern: `^(\\s+|export\\s(default\\s)?(async\\s)?)function`, // https://eslint.style/rules/js/max-len#ignorepattern
     }],
   },
 }
