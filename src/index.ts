82f002178e82de22cb9053d5010853590853eee1
Gábor Torma <gabor@torma.dev>
2024-09-13 10:15:43 +0200

    chore: upgrade all deps.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import config from '@antfu/eslint-config'
-import { defu } from 'defu'
 import type { OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
 import type { FlatConfigComposer } from 'eslint-flat-config-utils'
+import config from '@antfu/eslint-config'
+import { defu } from 'defu'
 import { defaultOptions, packageJsonRules, styleMaxLenRules } from './options'
 
 export default async function antfu(options?: OptionsConfig & TypedFlatConfigItem): Promise<FlatConfigComposer<TypedFlatConfigItem>> {
