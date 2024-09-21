6966cc89984a93567b9e277777191b099472b2ac
Gábor Torma <gabor@torma.dev>
2024-09-21 11:45:15 +0200

    fix: config types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import type { OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
+import type { Awaitable, ConfigNames, OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
+import type { Linter } from 'eslint'
 import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import config from '@antfu/eslint-config'
 import { defu } from 'defu'
 import { defaultOptions, packageJsonRules, styleMaxLenRules } from './options'
 
-export default async function antfu(options?: OptionsConfig & TypedFlatConfigItem): Promise<FlatConfigComposer<TypedFlatConfigItem>> {
+// eslint-disable-next-line ts/promise-function-async
+export default function antfu(options?: OptionsConfig & Omit<TypedFlatConfigItem, 'files'>, ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>[]): FlatConfigComposer<TypedFlatConfigItem, ConfigNames> {
   return config(
     defu(
       options,
@@ -12,5 +14,6 @@ export default async function antfu(options?: OptionsConfig & TypedFlatConfigIte
     ),
     packageJsonRules,
     styleMaxLenRules,
+    ...userConfigs,
   )
 }
