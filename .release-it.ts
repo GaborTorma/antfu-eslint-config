6f23f29433d77f82fb5cb6b51b757fe62efb3ad6
Gábor Torma <gabor@torma.dev>
2025-05-18 09:45:29 +0200

    fix: use release-it-pnpm

--- .release-it.ts
+++ .release-it.ts
@@ -1,4 +1,9 @@
 export default {
+  plugins: {
+    'release-it-pnpm': {
+      publishCommand: `pnpm publish --access public --no-git-checks --tag $tag`,
+    },
+  },
   git: {
     requireCommits: true,
     requireCommitsFail: false,
